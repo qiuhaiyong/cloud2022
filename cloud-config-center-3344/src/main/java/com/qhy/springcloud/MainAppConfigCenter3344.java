96c2778f82357852f4f97a8775ff1f478ab64684
qiuhaiyong <1293044240@qq.com>
2022-09-29 17:34:24 +0800

    a

---
+++ cloud-config-center-3344/src/main/java/com/qhy/springcloud/MainAppConfigCenter3344.java
@@ -0,0 +1,14 @@
+package com.qhy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+public class MainAppConfigCenter3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(MainAppConfigCenter3344.class, args);
+    }
+}
